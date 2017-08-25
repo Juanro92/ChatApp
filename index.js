5c8580df137e65a0f562bd65418c7dc1f5fd4756
Juanro92 <juanrobamapa@gmail.com>
2017-08-25 13:09:51 -0400

    Basic

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
-	console.log('a user connected');
-)};
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+   socket.on('chat messag', function(msg){
+   	io.emit('chat message', msg);
+   });
+   socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
